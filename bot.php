cf84fba18b48fdb84b78cc16429abd8ed56c992c
Jakapong7777 <Jakapong1432@gmail.com>
2017-03-24 12:59:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,10 +21,17 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'ATOM ได้รับข้อความแล้วคับ'
-			];
+				'text' => 'ATOM  ได้รับคำสั่ง 1 และได้เริ่มทำงาน'
+			];	
+			} else{
+				// Get replyToken
+			$replyToken = $event['replyToken'];
 
-				
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => 'ATOM  ได้รับคำสั่งอื่นจะไม่ดำเนินการใดๆทั้งสิ้น'
+			];	
 			}
 			
 			// Make a POST Request to Messaging API to reply to sender
